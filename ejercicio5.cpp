fedc133565ddfd2ac56957849ac99ddee68855a9
Eduardo Noodt <edunoodt@yahoo.com.ar>
2021-06-08 10:09:35 -0300

    Programa sencillo de entrada y salida de datos

---
+++ ejercicio5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int edad;
+    char sexo[10];
+    float altura;
+
+    cout << "Ingrese edad: "; cin >> edad;
+    cout << "Ingrese sexo: "; cin >> sexo;
+    cout << "Ingrese altura: "; cin >> altura;
+
+    cout << "\nSu edad es: " << edad << endl;
+    cout << "Su sexo es: " << sexo << endl;
+    cout << "Su altura es: " << altura << endl;
+
+
+
+
+
+
+    return 10;
+}
